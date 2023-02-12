bdcfd52aa28b39bba332086f408f4fb3bc126f48
hishovon <akshovon112@gmail.com>
2023-02-12 22:38:45 +0600

    first-commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,27 +4,30 @@ const withdrawField=document.getElementById('withdraw-field');
 const newWithdrawAmountString= withdrawField.value;
 const newWithdrawAmount=parseFloat(newWithdrawAmountString);
 
+
 const withdrawTotalElement=document.getElementById('withdraw-total');
 const previousWithdrawTotalString=withdrawTotalElement.innerText;
 const previousWithdrawTotal=parseFloat(previousWithdrawTotalString);
-const currentWithdrawTotal= previousWithdrawTotal + newWithdrawAmount;
 
-withdrawTotalElement.innerText=currentWithdrawTotal;
 
 const currentTotal = document.getElementById("balance-total");
 const currentBalanceTotalString=currentTotal.innerText;
 const currentBalanceTotal=parseFloat(currentBalanceTotalString);
+
+withdrawField.value="";
+if(newWithdrawAmount>currentBalanceTotal){
+    alert('Enter a valid number');
+    return;
+}
+
+const currentWithdrawTotal= previousWithdrawTotal + newWithdrawAmount;
+withdrawTotalElement.innerText=currentWithdrawTotal;
+
+
+
 const finalBalance= currentBalanceTotal - newWithdrawAmount;
 
 currentTotal.innerText=finalBalance;
-
-
-withdrawField.value="";
-
-
-
-
-
 
 
 
